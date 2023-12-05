e265be2fba84235e541aa949d0d45138abbc2e55
Diego Marrero Ferrera <diego.marrero104@alu.ulpgc.es>
2023-12-05 11:44:48 +0000

    prediction-provider module complete. Only JMSWeatherStore.java gives ActiveMQConnection problems.

---
+++ prediction-provider/src/main/java/marrero_ferrera_gcid_ulpgc/control/JMSWeatherStore.java
@@ -0,0 +1,29 @@
+package marrero_ferrera_gcid_ulpgc.control;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public record JMSWeatherStore(String topicName) implements WeatherStore {
+    private static final String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+    @Override
+    public void insertWeather(String jsonDataContainer) {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(url);
+
+        try {
+            Connection connection = (Connection) factory.createConnection();
+            connection.start();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Topic topic = session.createTopic(topicName);
+            MessageProducer producer = session.createProducer(topic);
+
+            TextMessage message = session.createTextMessage(jsonDataContainer);
+            producer.send(message);
+            System.out.println("JSON message sent to topic '" + topicName + "': '" + jsonDataContainer + "'");
+            connection.close();
+        } catch (JMSException | jakarta.jms.JMSException e) {
+            e.getCause();
+        }
+    }
+}
---
+++ prediction-provider/src/main/java/marrero_ferrera_gcid_ulpgc/control/Main.java
@@ -0,0 +1,48 @@
+package marrero_ferrera_gcid_ulpgc.control;
+
+import marrero_ferrera_gcid_ulpgc.model.*;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Timer;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        if (args.length < 2) {
+            System.out.println("Usage: java Main <API Key> <database path>");
+            System.exit(1);
+        }
+
+        String apiKey = args[0];
+        String topicName = args[1];
+
+        List<Location> locationList = Arrays.asList(
+        new Location(28.01f, -15.58f, "Gran Canaria", "Risco Prieto"),
+        new Location(29.234f, -13.5f, "La Graciosa", "Caleta del Sebo"),
+        new Location(29f,  -13.5f, "Lanzarote", "Costa Teguise"),
+        new Location(28.35f, -14.11f, "Fuerteventura", "Pájara"),
+        new Location(28.1f,  -17.12f, "La Gomera", "San Sebastián de La Gomera"),
+        new Location(28.8f,  -17.81f, "La Palma", "Bosque de Los Tilos"),
+        new Location(27.73f,  -18.05f, "El Hierro", "El Pinar"),
+        new Location(28.28f,  -16.64f, "Tenerife", "Parque Nacional del Teide"));
+
+        System.out.println("Task started and scheduled... \n(Ignore errors)");
+
+        Timer timer = new Timer();
+        short delay = 0;
+        long period = 6 * 60 * 60 * 1000;
+
+        for (Location location: locationList) {
+            timer.scheduleAtFixedRate(new Task(location, apiKey, topicName), delay, period);
+        }
+
+         */
+        Location location = new Location(28.01f, -15.58f, "Gran Canaria", "Risco Prieto");
+        WeatherController controller = new WeatherController();
+
+        Instant instant = Instant.parse("2023-12-05T12:00:00Z");
+        controller.getAndPublishWeatherData(location, "3c3aea5ce433b076c2f83b0c608896d8", "aa", instant);
+    }
+}
