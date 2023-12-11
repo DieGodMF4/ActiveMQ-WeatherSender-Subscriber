df3822e07cff58b7e7d34b48a56cf3d82b13d0d7
Diego Marrero Ferrera <diego.marrero104@alu.ulpgc.es>
2023-12-11 16:31:22 +0000

    FINAL COMMIT. FileEventStoreBuilder UPDATED!!

--- event-store-builder/src/main/java/marrero_ferrera_gcid_ulpgc/control/FileEventStoreBuilder.java
+++ event-store-builder/src/main/java/marrero_ferrera_gcid_ulpgc/control/FileEventStoreBuilder.java
@@ -1,10 +1,13 @@
 package marrero_ferrera_gcid_ulpgc.control;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,36 +27,44 @@ public class FileEventStoreBuilder implements EventStoreBuilder {
     @Override
     public void storeMessages(ArrayList<String> jsonStrings) throws MyReceiverException {
         ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = null;
-        try {
-            jsonNode = objectMapper.readTree(jsonStrings.get(0));
-        } catch (JsonProcessingException e) {
-            throw new MyReceiverException("An error occurred while parsing the json String.", e);
+        for (String jsonString : jsonStrings) {
+            JsonNode jsonNode = null;
+            try {
+                jsonNode = objectMapper.readTree(jsonString);
+            } catch (JsonProcessingException e) {
+                throw new MyReceiverException("An error occurred while parsing the json String.", e);
+            }
+
+            String source = getSource(jsonNode);
+            String date = extractDate(jsonNode);
+
+            String directoryPath = basePath + source + "/" + date;
+            String fileName = "events.events";
+            String fullPath = Paths.get(directoryPath, fileName).toString();
+
+            writeJsonToFile(fullPath, jsonString);
         }
-        String source = getSource(jsonNode);
-        String date = extractDate(jsonNode);
-
-        String directoryPath = basePath + source + "/" + date;
-        String fileName = "events.events";
-        String fullPath = Paths.get(directoryPath, fileName).toString();
-
-        writeJsonToFile(fullPath, jsonStrings);
     }
 
-    private void writeJsonToFile(String filePath, ArrayList<String> jsonStrings) throws MyReceiverException {
+    private void writeJsonToFile(String filePath, String jsonString) throws MyReceiverException {
         try {
             Path directoryPath = Paths.get(filePath).getParent();
             Files.createDirectories(directoryPath);
 
             File file = new File(filePath);
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.enable(SerializationFeature.INDENT_OUTPUT); // Enable pretty printing
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.enable(SerializationFeature.INDENT_OUTPUT); // Enable pretty printing
 
-            objectMapper.writeValue(file, jsonStrings);
+                if (file.length() > 0) {
+                    writer.newLine();
+                }
 
+                objectMapper.writeValue(writer, jsonString);
+            }
         } catch (IOException e) {
-            throw new MyReceiverException("An error occurred while introducing the String into the File.",e);
+            throw new MyReceiverException("An error occurred while introducing the Strings into the File.", e);
         }
     }
 
